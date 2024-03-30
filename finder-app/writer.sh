6c321a9af3b2c7c016bf0d9390d3e9a3ec124baf
Andrey <andrey.zargarov@gmail.com>
2024-03-30 19:15:42 +0000

    Added error handing in writer.sh

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -2,14 +2,32 @@
 
 FILENAME=''
 STR=''
-
-if [ $# -lt 2 ]
+if [ "$#" -lt 2 ]
 then
 	echo "use $0 filename string"
 	exit 1
 else
 	FILENAME=$1
 	STR=$2
+fi
+
+if ! [ -d ${FILENAME%/*} ]
+then
+	mkdir  ${FILENAME%/*} 2>/dev/null
+fi
+
+if  [ "$?" -ne 0 ]
+then
+	echo "Direcory ${FILENAME%/*} could not be created."
+	exit 1
+fi
+
+touch ${FILENAME} 2>/dev/null
+
+if  [ "$?" -ne 0 ]
+then
+	echo "File ${FILENAME} could not be created."
+	exit 1
 fi
 
 echo ${STR} > ${FILENAME}
