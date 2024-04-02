8c318eada9ed9f02a87be6c9e58509fb6fc5bce7
Andrey <andrey.zargarov@gmail.com>
2024-04-02 18:35:37 +0000

    log facility

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -3,20 +3,21 @@
 #include <syslog.h>
 
 int main(int argc, char *argv[]) {
+    openlog(NULL,0,LOG_USER);
     if (argc != 3) {
-        syslog(LOG_USER, "Incorrect number of arguments provided");
+        syslog(LOG_ERR, "Invalid Number of arguments: %d", argc);
         return 1;
     }
-
+ 
     char *filename = argv[1];
     char *string = argv[2];
-
+    
     FILE *file = fopen(filename, "w");
     if (file == NULL) {
-        syslog(LOG_USER, "Unable to open file %s", filename);
+        syslog(LOG_ERR, "Unable to open file %s", filename);
         return 1;
     }
-
+    
     fprintf(file, "%s\n", string);
     fclose(file);
     syslog(LOG_DEBUG, "Writing \"%s\" to %s", string, filename);
