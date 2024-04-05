74015428febada09e758f86b55555cd9eac1b9c3
Andrey <andrey.zargarov@gmail.com>
2024-04-05 20:07:44 +0000

    systemcalls

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,13 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+	int ret = system(cmd);
+	if (ret == -1) return false;
 
-    return true;
+	if(WIFEXITED(ret)){
+		return true;
+	}
+	return false;
 }
 
 /**
@@ -47,7 +52,7 @@ bool do_exec(int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
+    //command[count] = command[count];
 
 /*
  * TODO:
@@ -58,19 +63,32 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+	
 
+    int status;
+    pid_t pid = fork();
+    if (pid == -1)
+        return false;
+    else if (pid == 0 ){
+        execv(command[0], command);
+        exit(-1);
+    }
+    if (waitpid(pid, &status,0) == -1){
+        return false;
+	}
+    else if (WIFEXITED(status)){
+	if ( WEXITSTATUS(status) == 0)
+		return true;
+	else
+        	return false;
+	}
     va_end(args);
-
-    return true;
+    return false;
 }
 
-/**
-* @param outputfile - The full path to the file to write with command output.
-*   This file will be closed at completion of the function call.
-* All other parameters, see do_exec above
-*/
-bool do_exec_redirect(const char *outputfile, int count, ...)
-{
+/** * @param outputfile - The full path to the file to write with command output. * This file will be closed at completion of the function call. * All other parameters, see do_exec 
+above */ 
+bool do_exec_redirect(const char *outputfile, int count, ...) {
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -82,9 +100,7 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
     command[count] = NULL;
     // this line is to avoid a compile warning before your implementation is complete
     // and may be removed
-    command[count] = command[count];
-
-
+    //command[count] = command[count];
 /*
  * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
@@ -92,6 +108,26 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    pid_t pid;
+    int status;
+    pid = fork();
+    if (pid == -1)
+        return false;
+    if (pid == 0 ){
+        int fd = open(outputfile, O_WRONLY|O_CREAT, 0644);
+        if (fd < 0) exit(-1);
+        if (dup2(fd, 1) < 0){
+            exit(-1);
+        }
+        close(fd);
+        execv(command[0], command);
+        exit(-1);
+    }
+
+    if (waitpid(pid, &status,0) == -1)
+        return false;
+    else if (WIFEXITED(status))
+        return true;
 
     va_end(args);
 
--- examples/systemcalls/systemcalls.h
+++ examples/systemcalls/systemcalls.h
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <fcntl.h>
+
 
 bool do_system(const char *command);
 
